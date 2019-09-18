24a3820f1aba6fd2ef6d2e8e57ab1f84409b307c
Vijay Patel <35554905+Patelvijaykumar@users.noreply.github.com>
2019-09-18 17:30:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "web" {
   ami           = "${var.ami_id}"
-  instance_type = "${var.instancetype}"
+  instance_type = "${var.instance_type}"
 
   tags = {
     Name = "${var.tag}"
